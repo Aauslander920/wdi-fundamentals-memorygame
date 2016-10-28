5b615c23a6cffe3340e26d0978e034fedc315757
Abi Auslander <abi920@gmail.com>
2016-10-28 16:06:20 -0400

    created more game logic with if..else statements

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,4 +5,24 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+if (cardOne === cardTwo) {
+	alert('You found a match!');
+} 
+else {
+	alert('Sorry, try again.');
+}
+
+if (cardThree === cardFour) {
+	alert('You found a match!');
+}
+else {
+	alert('Sorry, try again.');
+}
+
+if (cardOne === cardThree) {
+	alert('You found a match!');
+}
+else {
+	alert('Sorry, try again.');
+}
 
